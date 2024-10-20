702fd8bd3a74bfa8b6caca273b8f4f9200f22b31
Patrick Kastner <patrick.kastner@gmail.com>
2024-10-20 13:18:12 -0400

    Create env.bat

---
+++ env.bat
@@ -0,0 +1,7 @@
+REM Create a new environment (optional, but recommended)
+conda create --name 24-fa-energyinbuildings-commercial python=3.11
+
+REM Activate the environment
+conda activate 24-fa-energyinbuildings-commercial
+
+pip install scikeras tensorflow pandas matplotlib seaborn
