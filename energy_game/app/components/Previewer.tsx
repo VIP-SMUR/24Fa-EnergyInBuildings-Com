262cb97e0bf68e280531c71b7dd156c7416bffb2
amahajan68 <amahajan68@gatech.edu>
2024-10-21 00:36:10 -0400

    removed energy_game

--- energy_game/app/components/Previewer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Previewer.module.css';
-
-export default function Previewer() {
-  return (
-    <div className={styles.previewer}>
-      <p>Building Geometry Previewer</p>
-    </div>
-  );
-}
